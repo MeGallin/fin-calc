3b187d68202b38b9ec409daa238467fbe393a479
Guy <me@garyallin.uk>
2018-09-30 11:48:59 +0100

    Various changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Routing, routingComponents } from './routing';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { LoansComponent } from './loans/loans.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {MaterialModule} from './material.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {Routing, routingComponents} from './routing';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {LoansComponent} from './loans/loans.component';
 
 @NgModule({
   declarations: [
@@ -18,9 +21,10 @@ import { LoansComponent } from './loans/loans.component';
     LoansComponent
   ],
   imports: [
-    BrowserModule, Routing, FormsModule
+    BrowserModule, Routing, FormsModule, MaterialModule, BrowserAnimationsModule, FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
